2e6b8ba0ad9bd3320cb4d44e0a5ee8d2b85c92f9
Blake Stoller <blake.stoller01@gmail.com>
2023-08-28 20:57:41 -0400

    work on layout of page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const runtime = "edge";
 export default function Home() {
   return (
     <div className="w-full h-full from-zinc-800 to-slate-800 bg-gradient-to-r">
-      <div className="text-center mx-auto pt-16">
+      <div className="container text-center mx-auto pt-16">
         <h1 className="text-3xl md:text-5xl  text-zinc-200 font-extrabold">
           Retain <span className="underline">Healthy</span> volunteers
         </h1>
