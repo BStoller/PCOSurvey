5c173d82765d32bd2d7205085170d211dec237ef
Blake Stoller <blake.stoller01@gmail.com>
2023-09-10 16:36:59 -0400

    update footer

--- components/custom/layout/footer.tsx
+++ components/custom/layout/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer className="bg-zinc-900 w-full flex justify-center py-4">
-            <p>PCO Survey</p>
+            <p>Sunday Trends</p>
         </footer>
     )
 }
