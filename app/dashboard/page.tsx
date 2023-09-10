e633f6330b36d61ba645736fb1080e3f8dfa7efc
Blake Stoller <blake.stoller01@gmail.com>
2023-09-10 18:07:26 -0400

    Fix bug

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,17 +4,20 @@ import { pcoFetch } from "@/lib/pcoFetch";
 
 // export const runtime = "edge";
 
-export default async function Dashboard() {
+const PER_PAGE = 100;
 
+export default async function Dashboard() {
   async function getData(page: number) {
     const req = await pcoFetch(
-      `https://api.planningcenteronline.com/services/v2/teams?per_page=100&include=service_type&offset=${
-        page * 100
+      `https://api.planningcenteronline.com/services/v2/teams?per_page=10&include=service_type&offset=${
+        page * PER_PAGE
       }`,
-       {
-        callbackUrl: '/dashboard'
-       }
+      {
+        callbackUrl: "/dashboard",
+      }
     );
+
+    console.log(req.url);
 
     const json = (await req.json()) as TeamsResponse;
 
@@ -23,21 +26,22 @@ export default async function Dashboard() {
 
   var { data, meta, included = [] } = await getData(0);
 
-  for (
-    var i = 0;
-    (i += Math.ceil(meta.total_count / 100 - 1));
-    i * 100 <= meta.total_count
-  ) {
+  for (var i = 1; i <= Math.ceil(meta.total_count / PER_PAGE); i++) {
     const { data: _data, included: _included } = await getData(i);
     data = data.concat(_data);
-    included = included?.concat(included);
+    included = included?.concat(
+      _included?.filter((x) => !included.map((x) => x.id).includes(x.id)) ?? []
+    );
   }
 
   return (
     <div className="max-w-fit mx-auto">
       <h1 className="text-xl mt-8 text-zinc-400">Select team to analyze</h1>
       <div className="max-w-fit mt-4">
-      <SelectTeamCommand serviceTypes={included} teams={data}></SelectTeamCommand>
+        <SelectTeamCommand
+          serviceTypes={included}
+          teams={data}
+        ></SelectTeamCommand>
       </div>
     </div>
   );
