7960c8abf3e290ee0b8ed0a6487e0369f0f5586e
Blake Stoller <blake.stoller01@gmail.com>
2023-09-22 17:42:09 -0400

    fixing heading

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export const runtime = "edge";
 
 export default function Home() {
   return (
-    <div className="w-full h-1/2 from-slate-700 to-slate-800 bg-gradient-to-r flex items-center">
-      <div className="container text-center mx-auto max-h-fit">
-        <h1 className="text-3xl md:text-5xl  text-zinc-200 font-extrabold">
-          Retain <span className="underline">Healthy</span> volunteers
+    <div className="w-fulls from-slate-700 to-slate-800 bg-gradient-to-r flex items-center">
+      <div className="container text-center mx-auto max-h-fit py-20">
+        <h1 className="text-5xl lg:text-8xl md:text-5xl  text-zinc-200 font-extrabold mx-auto">
+          Retain <span className="underline">Healthy</span> <br/> volunteers
         </h1>
         <p className="text-2xl dark:text-gray-400">
           Find insights in serving trends and take action
