b60efc756b02921b48f415f81d394d8ffdd7b2cd
Blake Stoller <blake.stoller01@gmail.com>
2023-09-28 17:53:16 -0400

    Further dev

--- app/person/[id]/layout.tsx
+++ app/person/[id]/layout.tsx
@@ -1,9 +1,13 @@
+import { StartEndSearchPicker } from "@/components/custom/searchDateRangePicker";
 import { ReactNode } from "react";
 
-export default function PersonLayout({children} : {children : ReactNode}) {
-    return (
-        <div className="container">
-            {children}
-        </div>
-    )
+export default function PersonLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="container">
+      <div className="flex lg:justify-end mt-4">
+        <StartEndSearchPicker></StartEndSearchPicker>
+      </div>
+      {children}
+    </div>
+  );
 }
